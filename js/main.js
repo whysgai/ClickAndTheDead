3b2cf85f5fa849ab24f4ab92189287a560d0c00d
Will Cohen <will.r.cohen@gmail.com>
2017-01-02 13:10:06 -0500

    Click counter finished.

---
+++ js/main.js
@@ -0,0 +1,61 @@
+var status = "Not started";
+var clicks = 0;
+var secondsLeft = 60;
+var nIntervalID;
+
+function clicking(){
+  if(status=="Running"){
+    clicks++;
+  }
+  else {
+    alert("Game is not running.");
+  }
+}
+function buttonSwitch(){
+  $("#clickButton").toggleClass("buttonLocked buttonGo");
+}
+function timer (secondsLeft){
+  console.log("Called timer: "+ secondsLeft);
+  $(".seconds").html(secondsLeft);
+}
+function clockStart () {
+  nIntervalID = setInterval(function(){
+    console.log("SET INTERVAL GO");
+    timer(secondsLeft);
+    if (secondsLeft == 0){
+      clockStop();
+      buttonSwitch();
+      alert("Time's up!");
+      postClicks();
+      status = "Not started";
+    }
+    else {
+      secondsLeft--;
+    }
+  },1000);
+}
+function clockStop () {
+  clearInterval(nIntervalID);
+}
+function postClicks(){
+  $("#clickDisplay").html(clicks+" clicks per minute");
+}
+
+
+$(document).ready(function(){
+  $(window).on("keydown", function(e){
+    if (e.which===32 && (status=="Not started" || status == "Paused")){
+      status = "Running";
+      buttonSwitch();
+      clockStart();
+    }
+    else if (e.which===32 && status=="Running") {
+      status = "Paused";
+      buttonSwitch();
+    }
+  });
+  $("#clickButton").on("click", function(e){
+    e.preventDefault;
+    clicking()
+  });
+})
