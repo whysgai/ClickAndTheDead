ba4568b9b77202668796cd79a74f7c0283c0dae1
Will Cohen <will.r.cohen@gmail.com>
2017-01-17 19:45:35 -0500

    averages and clearer instructions

--- js/clickDraw.js
+++ js/clickDraw.js
@@ -1,10 +1,14 @@
 var status = "Not started";
 var clickTime = 0;
-var waitDelay = Math.round(Math.random() * (2000 - 25) + 25);
+var waitDelay = 0;
 var secondsLeft = 3;
 var miliseconds = 0;
+var arrayClicks = [];
+var avgClicks = 0;
 var nIntervalID;
 var nIntervalID2;
+
+
 
 function buttonSwitch(){
   $("#clickButton").toggleClass("buttonLocked buttonGo");
@@ -28,7 +32,6 @@ function countdownStart () {
     }
   },1000);
 }
-
 function delayTimer () {
   nIntervalID = setInterval(function(){
     console.log("DelayTimer INTERVAL GO");
@@ -53,21 +56,44 @@ function clickTimer () {
 function stop (intVar) {
   clearInterval(intVar);
 }
+function calcAvg(){
+  arrayClicks.push(clickTime);
+  for (var i = 0; i < arrayClicks.length; i++){
+    avgClicks += arrayClicks[i];
+  };
+  avgClicks = avgClicks/arrayClicks.length;
+}
 function postClicks(){
   console.log(clickTime+" miliseconds");
   $("#clickDisplay").html(clickTime+" miliseconds");
+  $("#avgDisplay").html("Average: "+Math.round(avgClicks)+" miliseconds");
 }
+function reset(){
+  clickTime = 0;
+  waitDelay = Math.round(Math.random() * (2000 - 25) + 25);
+  secondsLeft = 3;
+  miliseconds = 0;
+  avgClicks = 0;
+}
+
 $(document).ready(function(){
+  reset();
   $(window).on("keydown", function(e){
-    if (e.which===32 && (status=="Not started" || status == "Paused")){
+    if (e.which===32 && status=="Not started") {
       status = "Running";
       countdownStart();
-
+    }
+    else if (e.which===32 && status=="Stopped") {
+      reset();
+      status = "Running";
+      countdownStart();
     }
   });
   $("#clickButton").on("click", function(e){
     e.preventDefault;
+    status = "Stopped";
     stop(nIntervalID2);
+    calcAvg();
     postClicks();
     buttonSwitch();
   });
